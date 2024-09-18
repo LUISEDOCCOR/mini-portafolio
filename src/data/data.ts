edd003fc75a55629a6367096bb75fb3e0ff6ac0c
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2024-09-17 21:46:06 -0600

    chore: format code and set eslit and prettier

--- src/data/data.ts
+++ src/data/data.ts
@@ -2,7 +2,7 @@ export const education = [
   {
     title: "Bachillerato",
     content: "Universidad de Guadalajara",
-    date: "2023 - 2026",
+    date: "2023 - 2026"
   },
   {
     title: "TAE de progrmación web",
@@ -11,8 +11,8 @@ export const education = [
         en la cual se ven temas relacionados con la programación
         web, programación funcional y programación orientada
         a objetos. Todo esto en un plazo de 2 años`,
-    date: "2024 - 2026",
-  },
+    date: "2024 - 2026"
+  }
 ];
 
 export const skills = [
@@ -21,40 +21,40 @@ export const skills = [
   "golang",
   "javascript/typescript",
   "astro",
-  "express-js",
+  "express-js"
 ];
 
 export const certificates = [
   {
     title: "Crea páginas web con HTML y CSS",
     content: "Curso sobre cómo crear y maquetar páginas usando HTML y CSS",
-    href: "https://www.mastermind.ac/certificates/67103e6b5b66470480a38a5f773caed1",
+    href: "https://www.mastermind.ac/certificates/67103e6b5b66470480a38a5f773caed1"
   },
   {
     title: "¡Linux: de Noob a Pro en 9 horas!",
     content: "Sistemas operativos basados en Debian Linux.",
-    href: "https://www.mastermind.ac/certificates/33290ab05fc14ed99e72c73613469b96",
+    href: "https://www.mastermind.ac/certificates/33290ab05fc14ed99e72c73613469b96"
   },
   {
     title: "Todo sobre la web con PHP",
     content: "Curso sobre cómo crear páginas usando PHP y MySQL",
-    href: "https://www.mastermind.ac/certificates/5448a06270fa4afdb8da70dabe1f2ce8",
+    href: "https://www.mastermind.ac/certificates/5448a06270fa4afdb8da70dabe1f2ce8"
   },
   {
     title: "Master en Python: Aprender Python 3, Django, Flask y Tkinter",
     content: "Curso sobre backend con django",
-    href: "https://udemy-certificate.s3.amazonaws.com/image/UC-d39174ac-83e2-4138-925a-16d4458b585b.jpg?v=1703626184000",
+    href: "https://udemy-certificate.s3.amazonaws.com/image/UC-d39174ac-83e2-4138-925a-16d4458b585b.jpg?v=1703626184000"
   },
   {
     title: "Frontend con HTML, CSS y JS",
     content: "Curso sobre cómo crear páginas usando HTML, CSS y JS",
-    href: "https://www.mastermind.ac/certificates/551bc00405864777873a31006c8b55b5",
+    href: "https://www.mastermind.ac/certificates/551bc00405864777873a31006c8b55b5"
   },
   {
     title: "Crea páginas web con HTML y CSS",
     content: "Curso sobre cómo crear y maquetar páginas usando HTML y CSS",
-    href: "https://www.mastermind.ac/certificates/67103e6b5b66470480a38a5f773caed1",
-  },
+    href: "https://www.mastermind.ac/certificates/67103e6b5b66470480a38a5f773caed1"
+  }
 ];
 
 export const proyects = [
@@ -63,7 +63,7 @@ export const proyects = [
     content:
       "Gestiona tus tareas diarias de manera eficiente, establece recordatorios y realiza un seguimiento de tu progreso.",
     href: "https://www.edutask.online/",
-    skills: ["tailwindcss", "supabase", "astro"],
+    skills: ["tailwindcss", "supabase", "astro"]
   },
   {
     title: "Recursos DEV",
@@ -71,13 +71,13 @@ export const proyects = [
       "Recursos para programadores, utiliza la IA para poder encontrar los que mejor se acomoden a tus necesidades.",
     href: "https://recursos-dev-ai.vercel.app/",
     skills: ["tailwindcss", "vite", "react", "express", "vercel-SDK", "prisma"],
-    isWorking: true,
+    isWorking: true
   },
   {
     title: "ColorKit",
     content:
       "Creador de paletas de colores para tailwindcss, en la que crea 11 colores, pero como usuario puedes escoger 2 colores y siempre te dará 11 colores que combinan entre ellos.",
     href: "https://www.colorkit.xyz/",
-    skills: ["tailwindcss", "react", "typescript"],
-  },
+    skills: ["tailwindcss", "react", "typescript"]
+  }
 ];
